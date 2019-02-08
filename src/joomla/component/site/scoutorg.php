0d9cffe7bcf87df540dca20b4dce9f6e4f0e3fec
Alexander Krantz <klako@hotmail.se>
2019-02-08 18:38:31 +0100

    Removed authorization for site side.

--- src/joomla/component/site/scoutorg.php
+++ src/joomla/component/site/scoutorg.php
@@ -1,10 +1,6 @@
 <?php
 defined('_JEXEC') or die('Restricted access');
 
-if (!JFactory::getUser()->authorise('core.manage', 'com_scoutorg')) {
-	throw new Exception(JText::_('JERROR_ALERTNOAUTHOR'));
-}
-
 $controller = JControllerLegacy::getInstance('ScoutOrg');
 
 $input = JFactory::getApplication()->input;
